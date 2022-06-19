b96ea1d0f20c3fa263d61daa98f3b64e5905372f
Karina <kabanova.karina@mail.ru>
2022-06-19 12:00:49 +0400

    completed

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,11 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    let arrCopy = arr.slice()
+    function sortArray(a, b) {
+        return a.localeCompare(b, 'ru', { caseFirst: 'upper', sensitivity: 'case' });
+    }
+    arrCopy.sort(sortArray);
+    if (param === 'desc') return arrCopy.reverse()
+    if (param === 'asc') return arrCopy;
 }
