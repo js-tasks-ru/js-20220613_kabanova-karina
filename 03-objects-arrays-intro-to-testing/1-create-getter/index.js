95ae9b7c221f93a9231b09e3f5fdd77c78ece885
Karina <kabanova.karina@mail.ru>
2022-06-21 15:41:24 +0400

    completed

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,16 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    let arr = path.split('.');
+    let lastValue = arr[arr.length - 1];
+    let coint = 0
+
+    return function foo(obj) {
+        coint++
+        if (coint === arr.length) return obj[lastValue];
+        for (let deep of Object.values(obj)) {
+            return foo(deep);
+        }
+    };
 
 }
