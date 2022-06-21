ac10e3c936a7766bbc6b448746daf17788821607
Karina <kabanova.karina@mail.ru>
2022-06-21 16:11:47 +0400

    completed

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,11 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    if (obj) {
+        let epmtyObject = {};
+        for (let [key, value] of Object.entries(obj)) {
+            epmtyObject[value] = key;
+        }
+        return epmtyObject;
+    }
 }
